57e9313bfd2a7911ab553ab8ecc5033cb1ad754b
taras131 <mossnabitkana@gmail.com>
2021-08-08 20:19:11 +0300

    add_login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,27 +1,20 @@
 const Router = require('express')
 const User = require('../models/User')
-const router = new Router()
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const {check, validateResult} = require('express-validator')
+const config = require('config')
 
-router.post('/registration',
- //   [
- //       check('email', "Uncorrect email").isEmail(),
-//        check('password', "Password longer uncorrect").isLength({min: 3, max: 12}),
-//   ],
-    async (req, res) => {
-    console.log(req.body)
+const router = new Router()
+router.post('/registration', async (req, res) => {
+        console.log(req.body)
         try {
-  //          const errors = validateResult(req)
-//            if(!errors.isEmpty()){
-  //              res.status(400).json({message: "Не корректные данные", errors})
-  //          }
             const {email, password} = req.body
             const candidate = await User.findOne({email})
             if (candidate) {
                 res.status(400).json({message: "Такой пользователь уже существует"})
             }
-            const hashPassword = await bcrypt.hash(password, 13)
+            const hashPassword = await bcrypt.hash(password, 5)
             const user = new User({email, password: hashPassword})
             await user.save()
             return res.json({message: "User was created"})
@@ -30,5 +23,31 @@ router.post('/registration',
             res.send({message: "server error"})
         }
     })
+
+router.post('/login', async (req, res) => {
+    console.log(req.body)
+    try {
+        const {email, password} = req.body
+        const user = await User.findOne({email})
+        if (!user) {
+            res.status(404).json({message: "User not found"})
+        }
+        const isPassValid = await bcrypt.compareSync(password, user.password)
+        if (!isPassValid) {
+            res.status(400).json({message: "Invalid password"})
+        }
+        const token = await jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn: "12h"})
+        return res.json({
+            token,
+            user: {
+                id: user.id,
+                email: user.email
+            }
+        })
+    } catch (e) {
+        console.log(e)
+        res.send({message: "server error"})
+    }
+})
 
 module.exports = router
